a9ee40cf9ca2f67a3631fa67e339abfb558f89d9
Apollo24601 <ememobong24601@gmail.com>
2023-03-29 22:49:23 +0100

    added weather icons and set current time

--- src/app.js
+++ src/app.js
@@ -1,47 +1,3 @@
-//current time code
-let h2 = document.querySelector("h2");
-let now = new Date();
-
-let date = now.getDate();
-let hour = now.getHours();
-if (hour < 10) {
-  hour = `0${hour}`;
-}
-
-let min = now.getMinutes();
-if (min < 10) {
-  min = `0${min}`;
-}
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-let month = months[now.getMonth()];
-
-h2.innerHTML = `${day}, ${date} ${month} ${hour}:${min}`;
-
 let apiKey = "a710bd8bd76400c9658ef649d9e81728";
 
 //to overwrite h1
@@ -60,6 +16,52 @@ function search(event) {
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSearch);
 
+//to set current date
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let dateNumber = date.getDate();
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+
+  let min = date.getMinutes();
+  if (min < 10) {
+    min = `0${min}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[date.getMonth()];
+
+  return `${day}, ${dateNumber} ${month} ${hour}:${minutes}`;
+}
+
 //weather description
 function showTemp(response) {
   console.log(response.data);
@@ -67,7 +69,7 @@ function showTemp(response) {
   h1.innerHTML = `${response.data.name}`;
 
   let weatherDesc = document.querySelector("#weather-description");
-  weatherDesc.innerHTML = response.data.weather[0].main;
+  weatherDesc.innerHTML = response.data.weather[0].description;
 
   //to access temperature
   let temperature = Math.round(response.data.main.temp);
@@ -76,12 +78,25 @@ function showTemp(response) {
 
   let maxTemp = document.querySelector("#high");
   maxTemp.innerHTML = `${response.data.main.temp_max}`;
+
   let minTemp = document.querySelector("#low");
   minTemp.innerHTML = `${response.data.main.temp_min}`;
+
   let windSpeed = document.querySelector("#wind");
   windSpeed.innerHTML = `${response.data.wind.speed}mph`;
+
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = `${response.data.main.humidity}%`;
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 //overwriting 33degrees
@@ -100,6 +115,7 @@ function showPosition(position) {
 }
 
 function currentCityWeather(response) {
+  console.log(response.data);
   let city = response.data.name;
   let h1 = document.querySelector("h1");
   h1.innerHTML = city;
